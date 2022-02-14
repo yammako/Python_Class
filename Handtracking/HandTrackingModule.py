d010c59e39dfa298452da5a9752a6b1bfb88a04d
Myungjin Cho <yammako@gmail.com>
2022-02-14 15:12:22 +0900

    second commit

--- Handtracking/HandTrackingModule.py
+++ Handtracking/HandTrackingModule.py
@@ -53,7 +53,8 @@ def main():
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
         cv2.imshow("Image", img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 if __name__ == "__main__":
     main()
